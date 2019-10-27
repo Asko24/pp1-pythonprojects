d2591d270da5d1a3b14f1ed1608ab65d56752b45
Asko24 <38815170+Asko24@users.noreply.github.com>
2019-10-27 18:47:35 +0100

    Delete 29.py

--- 29.py
+++
@@ -1,5 +0,0 @@
-import random
-cpu_roll=int(random.randint(1,6))
-player_guess=int(input('Podaj, ile oczek kostki wyrzucił komputer: '))
-print('Komputer wyrzucił: ',cpu_roll)
-print('Zgadłeś: ',cpu_roll==player_guess)
