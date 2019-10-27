4753276a7997b7d124ce3cc9b999bb0c2b8873ea
Asko24 <38815170+Asko24@users.noreply.github.com>
2019-10-27 18:46:48 +0100

    Delete 20.py

--- 20.py
+++
@@ -1,9 +0,0 @@
-'''
-Obliczanie pola powierzchni i obwodu koła o zadanym promieniu
-'''
-
-import math
-r=5
-obwod=2*math.pi*r
-pole=math.pi*r**2
-print(f'Pole koła wynosi: {pole}\nObwód koła wynosi: {obwod}')
